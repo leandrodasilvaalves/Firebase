b1408f9324ae2a1f1423ad3cf171827fce1a5a8c
Leandro Alves <leandro.silva.alves86@gmail.com>
2018-09-07 20:37:31 -0300

    Storage

---
+++ exemplo-balta-io/js/storage.js
@@ -0,0 +1,23 @@
+//elementos
+var uploader = document.getElementById("uploader");
+var fileButton = document.getElementById("fileButton");
+
+fileButton.addEventListener('change', function (e) {
+    var file = e.target.files[0];
+
+    var storageRef = firebase.storage().ref('arquivos/' + file.name);
+
+    var task = storageRef.put(file);
+
+    task.on('state_changed',
+        function progress(snapshot) {
+            var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            uploader.value = percentage;
+        },
+        function error(err) {
+            console.error(err);
+        },
+        function complete() {
+            alert('Upload completo');
+        });
+});
